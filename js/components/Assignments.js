b888031fe667f886e28a5b3fdc20aa609ad17587
moemen gaballah <moemengaballa@gmail.com>
2025-10-03 17:01:20 +0300

    Handle a form submission

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -1,25 +1,45 @@
 import AssignmentList from "./AssignmentList.js";
+
 export default {
     components: {AssignmentList},
     template: `
         <section class="space-y-6">
             <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
             <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+            
+            <form @submit.prevent="add">
+                <div class="border border-gray-600 text-black">
+                    <input v-model="newAssignment" placeholder="New assignment..." class="p-2"/>
+                    <button type="submit" class="bg-wight p-2 border-l">Add</button>
+                </div>
+            </form>
        </section>
     `,
 
     data() {
         return {
             assignments: [
-                { name: 'Finish project', complete: false, id: 1},
-                { name: 'Read Chapter 4', complete: false, id: 2},
-                { name: 'Turn in Homework', complete: false, id: 3},
-            ]
+                {name: 'Finish project', complete: false, id: 1},
+                {name: 'Read Chapter 4', complete: false, id: 2},
+                {name: 'Turn in Homework', complete: false, id: 3},
+            ],
+            newAssignment: '',
+        }
+    },
+    methods: {
+        add() {
+            this.assignments.push({
+                name: this.newAssignment,
+                completed: false,
+                id: this.assignments.length + 1
+            });
+
+            this.newAssignment = '';
         }
     },
     computed: {
         inProgress() {
-            return this.assignments.filter(assignemnt => ! assignemnt.complete)
+            return this.assignments.filter(assignemnt => !assignemnt.complete)
         },
         completed() {
             return this.assignments.filter(assignemnt => assignemnt.complete)
@@ -27,7 +47,7 @@ export default {
 
         filters() {
             return {
-                inProgress: this.assignments.filter(assignemnt => ! assignemnt.complete),
+                inProgress: this.assignments.filter(assignemnt => !assignemnt.complete),
                 completed: this.assignments.filter(assignemnt => assignemnt.complete),
             }
         }
